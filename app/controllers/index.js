1bfa4e76fe17e9cd13daec5f086648da113ad647
Bruno Pedroso <brunopedroso@gmail.com>
2016-08-31 08:58:13 -0300

    its creating messages

---
+++ app/controllers/index.js
@@ -0,0 +1,39 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  hoodieAccount: Ember.inject.service('hoodie-account'),
+  credentials: Ember.computed('username','password', function(){
+    return {
+      username: this.get('username'),
+      password: this.get('password')
+    };
+  }),
+
+  actions: {
+    signUp() {
+      this.get('hoodieAccount.signUp')(this.get('credentials')).then(()=>{
+        this.get('hoodieAccount.signIn')(this.get('credentials'));
+      }).catch((error)=>{
+        Ember.Logger.error(error);
+      });
+    },
+    signIn() {
+      this.get('hoodieAccount.signIn')(this.get('credentials')).then(()=>{
+        this.send("sessionChanged");
+      });
+
+    },
+    signOut() {
+      this.get('hoodieAccount.signOut')();
+    },
+
+
+    postNewMessage() {
+      this.store.createRecord('message', {text: this.get('newMessage')}).save();
+      this.set('newMessage', '');
+    },
+
+  },
+
+});
